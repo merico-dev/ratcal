d29b9cc59d27694cc272d751cbc2a485acf71dee
Jinglei Ren <jinglei@merico.dev>
2021-02-03 19:55:25 +0800

    Calculate H in calibrate()

--- ratcal/calibrate.py
+++ ratcal/calibrate.py
@@ -1,5 +1,32 @@
 import numpy as np
+from cvxopt import spmatrix
 
 
 def calibrate(M: np.array):
-    return M.shape
+    """
+    Calibrate ratings and evaluate raters.
+
+    :param M: The matrix of ratings.
+    :return:
+    """
+
+    if M.ndim != 2:
+        raise ValueError("M must be two-dimension, got %d dimension(s)" % M.ndim)
+    # m is the number of raters
+    # n is the number of objects being rated
+    m, n = M.shape
+
+    H = spmatrix([], [], [], (n + 2 * m, m * n))
+
+    for i in range(m):
+        for j in range(n):
+            if M[i, j] < 0 and M[i, j] != -1:
+                raise ValueError("M[%d, %d] = %f should be -1 denoting null" % (i, j, M[i, j]))
+
+            if M[i, j] >= 0:
+                rat = j * m + i
+                H[j, rat] = 1
+                H[n + i, rat] = -M[i, j]
+                H[n + m + i, rat] = 1
+
+    return H
--- tests/test_calibrate.py
+++ tests/test_calibrate.py
@@ -1,8 +1,36 @@
 import numpy as np
+import pytest
+
 from ratcal import calibrate
 
 
-def test_calibrate():
-    m = np.array([[2, 0, 2],
-                  [1, 0, 1]])
-    assert calibrate(m) == (2, 3)
+def test_input_dimensions():
+    m = np.zeros((2, 3))
+    calibrate(m)
+
+    with pytest.raises(ValueError) as error:
+        m = np.array([0, -1, 1])
+        calibrate(m)
+    assert 'got 1 dimension(s)' in str(error.value)
+
+    with pytest.raises(ValueError) as error:
+        m = np.zeros((1, 2, 3))
+        calibrate(m)
+    assert 'got 3 dimension(s)' in str(error.value)
+
+
+def test_input_ratings():
+    m = np.zeros((2, 3))
+    calibrate(m)
+
+    m = np.full((1, 5), -1.0)
+    calibrate(m)
+
+    m = np.random.rand(10, 1)
+    calibrate(m)
+
+    with pytest.raises(ValueError) as error:
+        m = np.array([[2, 1, 0.5],
+                      [5, -2, 1]])
+        calibrate(m)
+    assert 'should be -1 denoting null' in str(error.value)
